43fc8aeb01ee55fcf1a9ae5e5eddf37e52cb4d1c
lb madesia <lmadesia@gmail.com>
2021-03-03 12:23:35 +0530

    fullscreen

---
+++ public/lbasset/hrm/js/index.js
@@ -0,0 +1,53 @@
+// start left menu list angle icon control
+$(document).ready(function(){
+  $(".li-group-dropdown").click(function(){
+      var downDive = $(this).attr("aria-expanded");
+      if(downDive == "false"){
+          $(".angle-left").removeClass("fa-angle-left");
+          $(".angle-left").addClass("fa-angle-down");
+      }
+      else{
+        $(".angle-left").removeClass("fa-angle-down");
+        $(".angle-left").addClass("fa-angle-left");
+      }
+  });
+  
+
+  // start menu cog
+  $("#cog").click(function(){
+    var baction = $(this).attr("lbaction");
+    if(baction == "full"){
+      $(this).attr("lbaction","small");
+      $(".right-side").css({width: 'calc(100% - 40px)'});
+    }
+    else{
+      $(this).attr("lbaction","full");
+      $(".right-side").css({width: 'calc(100% - 250px)'});
+    }
+  });
+  // start full screen code
+  $("#fullscreen").click(function(){
+    var elem = document.documentElement;
+    var baction = $(this).attr("lbaction");
+    if(baction == "full"){
+      $(this).attr("lbaction","small");
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+      elem.msRequestFullscreen();
+    }
+  }
+  else{
+    $(this).attr("lbaction","full");
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+      document.msExitFullscreen();
+    }
+  }
+  });
+});
